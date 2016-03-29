e54d95c3da11bce2662ceab8d9b6a453eba1da0f
suppriya <suppriya@missionrnd.com>
2016-03-29 11:53:09 -0700

    Submit Commit

--- src/CountPairs.cpp
+++ src/CountPairs.cpp
@@ -23,13 +23,32 @@ NOTE : Donot use any Global Variables
 #include "stdafx.h"
 
 //You can use this function ,for the actual recursion .Think of similar functions for all other problems.
-int count_pairs(char *str, int len, int start, int end){
-	return 0;
+int count_pairs(char *str, int len, int start, int end,int count){
+	if (len == 0)
+	{
+		return 0;
+	}
+	if (str[start] == str[end])
+	{
+		count++;
+	}
+	else if (start == (len - 2))
+	{
+		return count;
+	}
+
+	if (start<len - 2)
+	{
+		return count_pairs(str, len, start + 1, end + 1, count);
+	}
+	
 }
 
 int count_pairs_wrapper(char *str,int len){
-    //Wrapper function which might call a recursive function ,which might take extra parameters .
-	return 0;
+	int res;
+    res = count_pairs(str, len, 0, 2, 0);
+	return res;
+	
 
 }
 
